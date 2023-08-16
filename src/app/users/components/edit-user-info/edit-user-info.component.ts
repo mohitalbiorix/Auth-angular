dbc93707af93e123dbd77cfee77ee53ae295ac1f
mohit <mohitk.albiorix@gmail.com>
2023-08-16 17:26:17 +0530

    Removed unused variable

--- src/app/users/components/edit-user-info/edit-user-info.component.ts
+++ src/app/users/components/edit-user-info/edit-user-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, Input, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { AuthService } from 'src/app/auth/services/auth.service';
